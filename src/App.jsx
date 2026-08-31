556070647af486c589f4edfc359af5beb66a5c01
Kaizen-Mage <simulationworld4@gmail.com>
2026-08-31 15:16:27 +0300

    PORT-4 Create About Page Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,21 @@
-import { Routes, Route, Link } from "react-router-dom";
+import { Routes, Route /*Link*/ } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Projects from "./pages/Projects";
 import Contact from "./pages/Contact";
-import  Profile  from "./pages/Profile";
+import BuildFooter from "./pages/BuildFooter";
+import Profile from "./pages/Profile";
+
 export default function App() {
    return (
       <div>
-         <nav>
+         {/* <nav>
             <Link to="/">Home</Link> | <Link to="/about">About</Link> |{" "}
             <Link to="/projects">Projects</Link> |{" "}
             <Link to="/contact">Contact</Link> |{" "}
             <Link to="/profile">Profile</Link>
          </nav>
-         <hr />
+         <hr /> */}
          <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/about" element={<About />} />
